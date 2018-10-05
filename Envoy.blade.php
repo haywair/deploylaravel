b7739c960361ca1ef3828a0da724095830463e61
Your Name <you@example.com>
2018-10-05 21:58:53 +0800

    envoy测试

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,8 +1,8 @@
 @servers(['web-1' => 'root@178.128.72.1', 'web-2' => 'root@178.128.76.137'])
 
 @task('deploy', ['on' => ['web-1', 'web-2'], 'parallel' => true])
-cd /var/www/deploylaravel
-git pull origin master
-composer install --no-dev
-php artisan migrate --force
+    cd /var/www/deploylaravel
+    git pull origin master
+    composer install --no-dev
+    php artisan migrate --force
 @endtask
